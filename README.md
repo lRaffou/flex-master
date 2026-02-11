4b8bc7a8dc064f23341bc08287464560d29ec81d
lRaffou <akira182@proton.me>
2026-02-11 12:15:04 +0100

    first center element flexbox

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Reprise avec test
 **Je me rend compte que j'ai du boulot à rattrapper à tout les niveaux j'ai même recherché comment link le scrip c'est abusé, bref lesssgo, tu vas y arriver.**
 
 Ah oui parce que je veux tout faire en JS j'veux être à l'aise avec.
+
+Je dois revoir les flexbox et ses propriétés, j'y avais passé beaucoups de temps avant c'est bien dommage ça raff.
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// Hello world! 
+// Hello world!
+const container = document.querySelector(".container");
+const item = document.querySelector(".item");
+const content = document.querySelector(".content");
+const title = document.querySelector("h1");
+const img = document.querySelector("img");
+
+//Je veux que mon item soit centré dans mon container
+container.style.padding = "20px";
+container.style.height = "100vh";
+container.style.display = "flex";
+container.style.justifyContent = "center";
+container.style.alignItems = "center";
+
+
+item.style.width = "25vw";
+item.style.height = "35vh";
+item.style.display = "flex";
+item.style.flexDirection = "column";
+item.style.justifyContent = "center";
+item.style.alignItems = "center";
+item.style.border = "2px solid white";
+item.style.borderRadius = "10px";
+
+content.style.width = "100%";
+content.style.height = "100%";
+
+img.style.width = "100%";
+img.style.height = "100%";
+// img.style.border = "2px solid white";
+// img.style.borderRadius = "10px";
+// img.style.objectFit = "contain";
